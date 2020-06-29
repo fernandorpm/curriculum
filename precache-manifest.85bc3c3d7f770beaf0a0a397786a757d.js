54cf2ca1604f78b654d3fd18d4adb08370243c82
Fernando R P Marques <frpm1001@gmail.com>
2020-06-29 01:13:19 -0300

    Updates

---
+++ precache-manifest.85bc3c3d7f770beaf0a0a397786a757d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41a004a4119c9e770a28f65db1f5a70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a7436441856ea97fe79",
+    "url": "/static/css/main.e7f048c2.chunk.css"
+  },
+  {
+    "revision": "1f48f77080a4deb83a29",
+    "url": "/static/js/2.868623be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.868623be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a7436441856ea97fe79",
+    "url": "/static/js/main.bd97f6e0.chunk.js"
+  },
+  {
+    "revision": "f96bc48569e2aa5deaf7",
+    "url": "/static/js/runtime-main.67fc2db9.js"
+  },
+  {
+    "revision": "abaaae3fc616ff37b8fa9654990d8288",
+    "url": "/static/media/CV_FernandoRPM.abaaae3f.pdf"
+  },
+  {
+    "revision": "1c7ec61228ed1d5049fd2710cc36216b",
+    "url": "/static/media/me.1c7ec612.png"
+  }
+]);
